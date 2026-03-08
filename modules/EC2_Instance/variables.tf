38eb526908e3e5050e0af6aa60604d04064a4dc0
varmaaradhula <rkvarmaa@gmail.com>
2026-03-08 00:31:45 +0000

    first commit

--- modules/EC2_Instance/variables.tf
+++ modules/EC2_Instance/variables.tf
@@ -1,29 +1,28 @@
 variable "public_key"{
 
-    type = "string"
+    type = string
     description = "public key value"
-    default = data.vault_kv_secret_v2.pubkey.data["public_key"]
 }
 
 variable "amiId"{
-    type = "string"
+    type = string
     description = "ami id value"
 
 }
 
 variable "instance_type" {
-    type = "string"
+    type = string
     description = "instance type value"
 
 }
 
 variable "sgname"{
-    type = "string"
+    type = string
     description = "security group name"
 }
 
 variable "tagname" {
-    type = "string"
+    type = string
     description = "tag name value"
 
 }
